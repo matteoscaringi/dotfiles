fb2b210b4f501420d477fbc8bfbf65f1edc964b7
DeadlyFamous06 <matteoscaringi1@gmail.com>
2021-10-24 17:38:52 +0200

    Added git aliases to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,32 @@
+#Onload of terminal/tty
 export EDITOR='nano'
 if status is-interactive
     pfetch
-    alias btw='neofetch'
     starship init fish | source
 end
-if status is-login
+
+#Login script for startx
+if status is-login	
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
     exec startx $HOME/.xinitrc
   end
 end
+
+#Git aliases
+alias g='git'
+alias ga='git add -u'
+alias gs='git status'
+alias gb='git branch'
+alias gc='git commit -v'
+alias gd='git diff'
+alias gp='git push'
+alias gr='git restore'
+alias gf='git fetch -p'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias glc='git log --oneline | wc -l'
+
+#Other aliases
+alias btw='neofetch'
+alias code='vscodium'
+alias weer='curl wttr.in'
